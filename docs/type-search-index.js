7c805baff9cfa379b8e13398d1979de8e7b590b2
eric-soto-bol <ericsoto5120@gmail.com>
2024-10-22 22:28:58 -0600

    Add files via upload
    javadoc

---
+++ docs/type-search-index.js
@@ -0,0 +1,1 @@
+typeSearchIndex = [{"l":"All Classes and Interfaces","u":"allclasses-index.html"},{"p":"poop10","l":"Cuenta"},{"p":"poop10","l":"POOP10"},{"p":"poop10","l":"saldoInsuficienteException"}];updateSearchResults();
